a3ea81893c6bbdb7da5a248ddccfb1d11231856a
Chloe Wahl-Dassule <cwahldassule c:; cd 'c:\Users\Chloe\OneDrive\Documents\CSC 120 files\CSC120-git-demo'; & 'C:\Users\Chloe\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\Chloe\.vscode\extensions\ms-python.python-2022.20.2\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '61049' '--' 'extension-output-vscode.git-#1-Git' @smith.edu>
2023-02-13 23:13:58 -0500

    mirroring works

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,60 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
+class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Integer rounds = 0;
+    String statement = ":)";
+    Scanner in;
+    Scanner in1;
+    Integer temp = 0;
+    String response = "";
+    String word;
+    //Boolean ans;
+    ArrayList<String> words = new ArrayList<>();
+    ArrayList<String> transcript = new ArrayList<>();
+
+    in = new Scanner(System.in);
+    System.out.println("How many rounds do you want to play?");
+    rounds = in.nextInt();
+
+    System.out.println("Hey! What's up?");
+
+    while (temp < rounds){
+      words.clear();
+      statement = "";
+      response = "";
+      in1 = new Scanner(System.in);
+      statement = in1.nextLine();
+      String str[] = statement.split(" ");
+      //ans = str.contains("You");
+        for (int i = 0; i <str.length; i++){
+          word = str[i];
+          if(word.equals("I")){
+            word = word.replace("I", "you");}
+          else if(word.equals("me")){
+            word = word.replace("me", "you");}
+          else if(word.equals("am")){
+            word  = word.replace("am", "are");}
+          else if(word.equals("you")){
+            word = word.replace("you", "I");}
+          else if(word.equals("my")){
+            word = word.replace("my", "your");}
+          else if(word.equals("your")){
+            word = word.replace("your", "my");}
+          words.add(word);
+          }
+
+      response = String.join(" ", words);
+      System.out.print(response + "?");
+      transcript.add(statement);
+      transcript.add(response + "?");
+      temp += 1;
+    }
+    
+    System.out.println("Transcript:");
+    System.out.println(transcript);
+    }
   }
-}
+//
