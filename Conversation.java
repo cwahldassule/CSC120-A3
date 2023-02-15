aea299dea36f092a05e5c412fba9712dffb83d55
Chloe Wahl-Dassule <cwahldassule c:; cd 'c:\Users\Chloe\OneDrive\Documents\CSC 120 files\CSC120-git-demo'; & 'C:\Users\Chloe\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\Chloe\.vscode\extensions\ms-python.python-2022.20.2\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '61049' '--' 'extension-output-vscode.git-#1-Git' @smith.edu>
2023-02-14 23:02:57 -0500

    Done!

--- Conversation.java
+++ Conversation.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
-
 class Conversation {
+  /**
+   * Chat bot has convorsation with user
+   * @param arguments
+   */
   public static void main(String[] arguments) {
-    Integer rounds = 0;
+    int rounds = 0;
     String statement = ":)";
     Scanner in;
     Scanner in1;
-    Integer temp = 0;
+    int temp = 0;
     String response = "";
     String word;
-    //Boolean ans;
+    Boolean ans = false;
+    Random rand = new Random();
+    ArrayList<String> rand_res = new ArrayList<>(Arrays.asList("wow!","That's crazy!",
+                                                              "I understand","... okay?", 
+                                                              "I don't know about that","Are you sure?","Amazing!"
+                                                              ));
     ArrayList<String> words = new ArrayList<>();
     ArrayList<String> transcript = new ArrayList<>();
 
@@ -19,42 +28,60 @@ class Conversation {
     System.out.println("How many rounds do you want to play?");
     rounds = in.nextInt();
 
-    System.out.println("Hey! What's up?");
+    System.out.println("Bot: Hey! What's up?");
 
     while (temp < rounds){
+      ans = false;
       words.clear();
       statement = "";
       response = "";
       in1 = new Scanner(System.in);
       statement = in1.nextLine();
-      String str[] = statement.split(" ");
-      //ans = str.contains("You");
-        for (int i = 0; i <str.length; i++){
-          word = str[i];
-          if(word.equals("I")){
-            word = word.replace("I", "you");}
-          else if(word.equals("me")){
-            word = word.replace("me", "you");}
-          else if(word.equals("am")){
-            word  = word.replace("am", "are");}
-          else if(word.equals("you")){
-            word = word.replace("you", "I");}
-          else if(word.equals("my")){
-            word = word.replace("my", "your");}
-          else if(word.equals("your")){
-            word = word.replace("your", "my");}
-          words.add(word);
-          }
-
-      response = String.join(" ", words);
-      System.out.print(response + "?");
       transcript.add(statement);
-      transcript.add(response + "?");
-      temp += 1;
-    }
-    
+      String str[] = statement.split(" ");
+      for (int a = 0; a <str.length; a++){
+        if(str[a].equals("I")
+            ||str[a].equals("me")
+            ||str[a].equals("am")
+            ||str[a].equals("you")
+            ||str[a].equals("my")
+            ||str[a].equals("your")
+            ||str[a].equals("I'm")){
+              ans = true;}}
+        
+        if(ans.equals(true)){
+          for (int i = 0; i <str.length; i++){
+            word = str[i];
+            if(word.equals("I")){
+              word = word.replace("I", "you");}
+            else if(word.equals("me")){
+              word = word.replace("me", "you");}
+            else if(word.equals("am")){
+              word  = word.replace("am", "are");}
+            else if(word.equals("you")){
+              word = word.replace("you", "I");}
+            else if(word.equals("my")){
+              word = word.replace("my", "your");}
+            else if(word.equals("your")){
+              word = word.replace("your", "my");}
+            else if(word.equals("I'm")){
+                word = word.replace("I'm", "you're");}
+            else if(word.equals("you're")){
+                  word = word.replace("you're", "I'm");}
+            words.add(word);}
+            response = String.join(" ", words);
+            System.out.println("Bot: " + response + "?");
+            transcript.add("bot:"+ response + "?");}
+
+        
+        else if(ans.equals(false)){
+          response = rand_res.get(rand.nextInt(7));
+          System.out.println("Bot: " + response);
+          transcript.add("Bot: " + response);}
+      temp += 1;}
+    System.out.println("");
     System.out.println("Transcript:");
     System.out.println(transcript);
-    }
-  }
-//
+    in.close();
+      }}
+    
--- reflection.md
+++ reflection.md
@@ -1,7 +1,14 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment? Rachel Knell, Ashby Steward-Nolan
+- Which classmates did you consult with while working on this assignment? 
+    Rachel Knell, Ashby Steward-Nolan
 - Which session(s) of TA / office hours did you attend?
+    Sunday
 - What are your initial impressions of Java? 
-- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    I like object oriented programming as a method of thinking and programming but I am annoyed that Java isn't as "laid back" as python is
+- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages?
+    Java is good at what it does but the programmer has to put more work in so that Java can run more effectively. Python feels like more of a free for all which can be good but not as efficient.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    It worked to think about what I would do in python and research what the syntax would be in Java. TA hours were helpful to get started so it's good to go early.
+
+    Somewhat unrelated: It was very helpful to do an object oriented assignment in python before we went to Java because it wasn't too much change all at once. The assignment might have even more value if we then rewrote the program in Java in class before doing the next assignment.
